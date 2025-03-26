755f444a0185f50f0c56c079040c45462a621f77
hijjoy <hijjoy@naver.com>
2025-03-26 23:48:32 +0900

    zustand useShallow

--- code1-state/src/components/IncrementButton.tsx
+++ code1-state/src/components/IncrementButton.tsx
@@ -1,8 +1,16 @@
 import { useStore } from "../store";
+// import { useShallow } from "zustand/react/shallow";
 
 export default function IncrementButton() {
   const handleClick = useStore((state) => state.increment); // 특정 값만 구독
   // const { increment } = useStore() count 가 변하면 불필요한 리랜더링 발생
+
+  // 여러값 가져올 때 useShallow 이용
+  // const { increment } = useStore(
+  //   useShallow((state) => ({
+  //     increment: state.increment,
+  //   }))
+  // );
 
   console.log("IncrementButton가 렌더링 되나요?");
 
